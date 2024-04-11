55de14eb08962bb64b299d404bf83f7343be3ac6
Aneew <d.gagyor@seznam.cz>
2024-04-11 21:34:40 +0200

    next commit

--- pages/insert1.php
+++ pages/insert1.php
@@ -13,9 +13,9 @@
 	            <h1 style="text-align: center;">Menu</h1>
                 <li><a href="../index.php">Main</a></li>
                 <!-- <li><a href="#" onclick="loadPage('pages/insert.php')">Insert</a></li> -->
-                <li><a href="#" onclick="loadPage('view.php')">View</a></li>
+                <li><a href="view.php">View</a></li>
                 <!-- <li><a href="#" onclick="loadPage('pages/viewprograms.php')">ViewPrograms</a></li> -->
-                <li><a href="#" onclick="loadPage('edit.php')">Edit</a></li>
+                <li><a href="edit.php">Edit</a></li>
                 <li><a href="insert1.php">Insert do DB</a></li>
                 <li><a href="result-counting.php">Vypocet</a></li>
 
